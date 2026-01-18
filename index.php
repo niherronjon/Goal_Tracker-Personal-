89be022ef6473ba9ed3c6060545f5f607bd5a56f
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2026-01-18 11:21:20 +0600

    Update index.php

--- index.php
+++ index.php
@@ -15,11 +15,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Create database if not exists in db
+// Create database if not exists in database
 $conn->query("CREATE DATABASE IF NOT EXISTS $database");
 $conn->select_db($database);
 
 // Create tables in database
+
 $conn->query("CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
